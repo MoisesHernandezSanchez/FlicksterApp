15ae9e597379ee6b82afeca30d143029c1b1b779
Moises Hernandez Sanchez <telekineticjake@gmail.com>
2021-02-19 13:40:52 -0500

    Initial commit of Flickster app

---
+++ app/src/main/java/com/example/flickster/adapters/MovieAdapter.java
@@ -0,0 +1,75 @@
+package com.example.flickster.adapters;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.flickster.R;
+import com.example.flickster.models.Movie;
+
+import java.util.List;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
+
+    Context context;
+    List<Movie> movies;
+
+    public MovieAdapter(Context context, List<Movie> movies) {
+        this.context = context;
+        this.movies = movies;
+    }
+
+    // Inflates layout and returns the holder
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View movieView = LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false);
+        Log.d("MovieAdapter", "onCreateViewHolder");
+        return new ViewHolder(movieView);
+    }
+
+    // Populates data into item through holder
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Log.d("MovieAdapter", "onBindViewHolder, position " + position);
+        // get movie at current position
+        Movie movie = movies.get(position);
+        // bind movie data into viewholder
+        holder.bind(movie);
+    }
+
+    // Returns item count in list
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView tvTitle;
+        TextView tvOverview;
+        ImageView ivPoster;
+
+        // creates relation from items in item_movie.xml to this ocument
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvTitle = itemView.findViewById(R.id.tvTitle);
+            tvOverview = itemView.findViewById(R.id.tvOverview);
+            ivPoster = itemView.findViewById(R.id.ivPoster);
+        } // end of public ViewHolder method
+
+        // sets title, overview, and image
+        public void bind(Movie movie) {
+            tvTitle.setText(movie.getTitle());
+            tvOverview.setText(movie.getOverview());
+            Glide.with(context).load(movie.getPosterPath()).into(ivPoster);
+        } // end of void bind
+    } // end of public class ViewHolder
+} // end of public class MovieAdapter
