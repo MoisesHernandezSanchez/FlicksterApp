32dc64cf28101fe442083232e237ee5da6905221
Moises Hernandez Sanchez <telekineticjake@gmail.com>
2021-02-26 10:02:19 -0500

    Added landscape orientation mode which uses backdrop instead of poster

--- app/src/main/java/com/example/flickster/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/flickster/adapters/MovieAdapter.java
@@ -1,12 +1,14 @@
 package com.example.flickster.adapters;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -57,7 +59,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
         TextView tvOverview;
         ImageView ivPoster;
 
-        // creates relation from items in item_movie.xml to this ocument
+        // creates relation from items in item_movie.xml to this document
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             tvTitle = itemView.findViewById(R.id.tvTitle);
@@ -69,7 +71,27 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
         public void bind(Movie movie) {
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverview());
-            Glide.with(context).load(movie.getPosterPath()).into(ivPoster);
+            String imageUrl;
+            if (context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                imageUrl = movie.getBackdropPath();
+            }
+            else {
+                imageUrl = movie.getPosterPath();
+            }
+            // if phone is in landscape, imageUrl = backdrop image
+            // else imageUrl = poster image
+
+            Glide.with(context).load(imageUrl).into(ivPoster);
+
+            // everything after this line is part of the unit 2
+            // 1. register click listener on whole container
+            // 2. navigate to new activity on tap
+            tvTitle.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(context, movie.getTitle(), Toast.LENGTH_SHORT).show();
+                }
+            });
         } // end of void bind
     } // end of public class ViewHolder
 } // end of public class MovieAdapter
